4e31a5cec43f9e04b246985a4031a0c28acfa95f
Kavya <kavyagsk1114@gmail.com>
2026-08-10 12:34:02 +0530

    First Java Program

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,36 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        // My Name
+        System.out.println("My Name");
+        System.out.println("G Kavya Sree");
+
+        // What I Learned in Foundation Month
+        System.out.println("\nWhat I Learned in Foundation Month");
+        System.out.println("1. I learned the basics of programming.");
+        System.out.println("2. I learned how to solve problems using logical thinking.");
+        System.out.println("3. I learned the basics of Git and GitHub.");
+        System.out.println("4. I improved my communication and problem-solving skills.");
+
+        // Why I Selected Java
+        System.out.println("\nWhy I Selected Java");
+        System.out.println("I selected Java because it is widely used in the software industry.");
+        System.out.println("It helps me build a strong foundation in programming and object-oriented programming.");
+
+        // My Career Goal
+        System.out.println("\nMy Career Goal");
+        System.out.println("My goal is to become a Software Developer.");
+
+        // What I Understood Today
+        System.out.println("\nWhat I Understood Today");
+
+        System.out.println("Programming means: Giving instructions to a computer to solve problems.");
+
+        System.out.println("Java is: A popular, object-oriented programming language.");
+
+        System.out.println("In this track, I will learn: Java programming, OOP concepts,");
+        System.out.println("data structures, problem solving, and application development.");
     }
 }
